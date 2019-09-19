561f928a080bf5927e292aeb409b689f4960620b
Israel Aguilar <iaguilaror@gmail.com>
2019-09-18 20:35:42 -0500

    NF: add help and version functions and write basic doc in nf script

---
+++ runtest.sh
@@ -0,0 +1,10 @@
+echo -e "======\n Testing NF execution \n======" \
+&& rm -rf test/results/ \
+&& nextflow run summarize-vcf.nf \
+	--vcffile test/data/sample.vcf.gz \
+	--metadata test/metadata/sample_ethnic_relation.tsv \
+	--output_dir test/results \
+	-resume \
+	-with-report test/results/`date +%Y%m%d_%H%M%S`_report.html \
+	-with-dag test/results/`date +%Y%m%d_%H%M%S`.DAG.html \
+&& echo -e "======\n VCF summarizer: Basic pipeline TEST SUCCESSFUL \n======"
